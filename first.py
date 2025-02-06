be552de1e0a8cdf4b2b2c23fa8aa5599b6abcee7
T Naveen <naveenroyal.t.72@gmail.com>
2025-02-06 15:46:05 +0530

    learned about if and else conditions

--- first.py
+++ first.py
@@ -1,19 +1,20 @@
-
 from flask import Flask,render_template
-
 app=Flask(__name__)
-
+db=[
+    {'author':'John Greesham','date_of_published':'09-10-2002',
+     'title':'How to success without any struggle'
+     },
+    {
+    'author':'Balayya','date_of_published':'09-10-1995',
+    'title':'Dont trouble the trouble'
+    }
+]
 @app.route('/')
-@app.route('/hello')
+
 def home():
-    return render_template('home.html')
+    return render_template('home.html',posts=db)
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html',title='About US')
 if __name__== "__main__":
     app.run(debug=True)
-why we should use render_templates
-Final Thought:
-render_template() is essential for building Flask web applications efficiently.
-It helps in dynamic content rendering, code reusability, and keeping Python code clean.
-    
